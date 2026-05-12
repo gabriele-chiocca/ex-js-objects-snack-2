a64d29632885b0fb01311a96a834c14a94eeccf8
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-12 18:32:43 +0200

    Question 2 Answer

--- script.js
+++ script.js
@@ -11,3 +11,18 @@
 //Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
 //Vi è solo un oggetto ai quali fanno riferimento hamburger e secondBurger
+
+//Question 2
+
+/*
+
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+
+console.log(hamburger.ingredients[0]); RISPOSTA "Salad"
+console.log(secondBurger.ingredients[0]); RISPOSTA "Salad"
+
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+Ci sono due oggetti realizzati l'oggetto principale al quale si fa riferimento in hamburger e la shallow copy creata in secondBurger
+
+*/
