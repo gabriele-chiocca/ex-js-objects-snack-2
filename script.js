09fbb2668cdae211d9708030c907f86a311482bd
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-12 18:28:26 +0200

    Question 1 Answer

---
+++ script.js
@@ -0,0 +1,13 @@
+//Risposte
+
+//Question 1
+
+//Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+
+//RISPOSTA
+console.log(hamburger.name); // "Double Cheese Burger"
+console.log(secondBurger.name); // "Double Cheese Burger"
+
+//Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+//Vi è solo un oggetto ai quali fanno riferimento hamburger e secondBurger
